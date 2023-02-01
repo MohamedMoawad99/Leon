764e57056163ca17b6817c74f1a4d5b03ad22350
Eng Mohamed <95402814+MohamedMoawad99@users.noreply.github.com>
2023-02-01 18:05:23 +0200

    Add files via upload

---
+++ js/javaScript_index.js
@@ -0,0 +1,8 @@
+function navLinks() {
+  var x = document.getElementById("LinksOfNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
